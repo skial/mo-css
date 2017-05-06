51dff9b8cb78f2891092a3ffc9a37c5897c59159
Skial Bainn <skial.bainn@gmail.com>
2017-05-06 14:58:28 +0100

    Move two parsers to mo-css

---
+++ src/uhx/mo/nth/Parser.hx
@@ -0,0 +1,44 @@
+package uhx.mo.nth;
+
+import haxe.io.Eof;
+import byte.ByteData;
+import uhx.mo.css.Lexer;
+import uhx.mo.css.Lexer.NthExpressions;
+
+class NthExpression {
+	
+	public function new() {
+		
+	}
+	
+	public function toTokens(bytes:ByteData, name:String):NthExpressions {
+		var lexer = new Lexer(bytes, name);
+		var tokens = [];
+		
+		try while ( true ) {
+			tokens.push( lexer.token( Lexer.nthExpression ) );
+		} catch (e:Eof) {
+			
+		} catch (e:Dynamic) {
+			trace( e );
+		}
+		
+		//if (tokens.length == 0) return Unknown;
+		//trace( bytes, tokens[0] );
+		return switch tokens[0] {
+			case Notation(a, b, isNegative):
+				if (a == 0 && b > 0) {
+					Index(b);
+					
+				} else {
+					tokens[0];
+					
+				}
+				
+			case _:
+				tokens[0];
+		}
+		
+	}
+	
+}
